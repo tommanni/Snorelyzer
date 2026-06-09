1618e6f102a1ed8a75f8465be30e5134b3bbd247
Tomi Manninen <tomi.manninen@tuni.fi>
2026-06-09 15:04:13 +0300

    Set Android navigation bar to app navigation bar theme color

--- app/src/main/java/com/example/snorelyzer/ui/theme/Theme.kt
+++ app/src/main/java/com/example/snorelyzer/ui/theme/Theme.kt
@@ -1,15 +1,22 @@
 package com.example.snorelyzer.ui.theme
 
 import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -52,7 +59,37 @@ fun SnorelyzerTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+        typography = Typography
+    ) {
+        SystemNavigationBarColor(darkTheme = darkTheme)
+        content()
+    }
 }
+
+@Composable
+private fun SystemNavigationBarColor(darkTheme: Boolean) {
+    val navigationBarColor = NavigationBarDefaults.containerColor
+    val view = LocalView.current
+
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = view.context.findActivity()?.window ?: return@SideEffect
+
+            @Suppress("DEPRECATION")
+            window.navigationBarColor = navigationBarColor.toArgb()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                window.isNavigationBarContrastEnforced = false
+            }
+
+            WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars =
+                !darkTheme
+        }
+    }
+}
+
+private tailrec fun Context.findActivity(): Activity? =
+    when (this) {
+        is Activity -> this
+        is ContextWrapper -> baseContext.findActivity()
+        else -> null
+    }
